02b4232cc5fc6d2eb47c11bd4f69b5b38e59aaac
atoivat <otavio.leite.ufu@gmail.com>
2019-12-10 20:53:26 -0300

    ae

--- main.c
+++ main.c
@@ -124,7 +124,7 @@ void toca(tipofila *F)
 
 int volta(tipofila *F)
 {
-    if (*F == NULL || (*F)->indicador == (*F)->ini)
+    if (fila_vazia(*F)|| (*F)->indicador == (*F)->ini)
     {
         return 0;
     }
